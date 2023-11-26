1f6c9b63e73b7014debd8f77282c8686228af275
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2023-11-26 20:34:40 -0300

    Update Main.java

--- src/com/gamesflix/Main.java
+++ src/com/gamesflix/Main.java
@@ -1,13 +1,28 @@
 package com.gamesflix;
 
+import com.gamesflix.controlers.PlayList;
+import com.gamesflix.models.MobileGames;
+import com.gamesflix.models.PcGames;
+import com.gamesflix.models.Games;
+
 public class Main {
 
     public static void main(String[] args) {
-        Games doom = new Games("Doom", "Shooter", 1993, false);
+        PcGames doom = new PcGames("Doom", "Shooter", 1993, false, true, false);
         doom.rate(10);
         doom.rate(8);
         doom.rate(7.5);
         doom.getDataSheet();
-    }
 
+        MobileGames candyCrush = new MobileGames("Candy Crush", "Match Three", 2012, true, true, true);
+        candyCrush.rate(9);
+        candyCrush.rate(8);
+        candyCrush.rate(7);
+        candyCrush.getDataSheet();
+
+        PlayList playList = new PlayList();
+        playList.addToList(doom);
+        playList.addToList(candyCrush);
+        playList.showList();
+    }
 }
