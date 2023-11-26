d0a39705da2f0c28440d16e4b85ac69746edfc73
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2023-11-26 20:34:30 -0300

    Create Games.java

---
+++ src/com/gamesflix/models/Games.java
@@ -0,0 +1,86 @@
+package com.gamesflix.models;
+
+public class Games {
+    private String title;
+    private String genre;
+    private int launchYear;
+    private double ratingsSum;
+    private int ratingsQuantity;
+    private boolean subscriptionNeeded;
+
+    public Games(String title, String genre, int launchYear, boolean subscriptionNeeded) {
+        this.title = title;
+        this.genre = genre;
+        this.launchYear = launchYear;
+        this.subscriptionNeeded = subscriptionNeeded;
+    }
+
+    public void getDataSheet() {
+        String text = """
+                Título: %s
+                Gênero: %s
+                Ano de lançamento: %d
+                Nota: %.1f
+                Quantidade de avaliações: %d
+                Necessário assinatura: %b
+                """.formatted(getTitle(), getGenre(), getLaunchYear(), getRatingAvarage(), getRatingsQuantity(), isSubscriptionNeeded());
+        System.out.println(text);
+    }
+
+    public void rate(double rating) {
+        ratingsSum += rating;
+        ratingsQuantity++;
+    }
+
+    public double getRatingAvarage() {
+        return ratingsSum / ratingsQuantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    protected String getGenre() {
+        return genre;
+    }
+
+    private void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    protected int getLaunchYear() {
+        return launchYear;
+    }
+
+    private void setLaunchYear(int launchYear) {
+        this.launchYear = launchYear;
+    }
+
+    protected boolean isSubscriptionNeeded() {
+        return subscriptionNeeded;
+    }
+
+    private void setSubscriptionNeeded(boolean subscriptionNeeded) {
+        this.subscriptionNeeded = subscriptionNeeded;
+    }
+
+    private double getRatingsSum() {
+        return ratingsSum;
+    }
+
+    private void setRatingsSum(double ratingsSum) {
+        this.ratingsSum = ratingsSum;
+    }
+
+    protected int getRatingsQuantity() {
+        return ratingsQuantity;
+    }
+
+    private void setRatingsQuantity(int ratingsQuantity) {
+        this.ratingsQuantity = ratingsQuantity;
+    }
+}
