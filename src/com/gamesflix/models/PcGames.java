047c9df4f4e7b5ac94a366e01c9c58415fa22ec6
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2023-11-26 20:34:34 -0300

    Create PcGames.java

---
+++ src/com/gamesflix/models/PcGames.java
@@ -0,0 +1,35 @@
+package com.gamesflix.models;
+
+public class PcGames extends Games {
+    private boolean mouseKeyboard;
+    private boolean joystick;
+
+    public PcGames(String title, String genre, int launchYear, boolean subscriptionNeeded, boolean mouseKeyboard, boolean joystick) {
+        super(title, genre, launchYear, subscriptionNeeded);
+        this.mouseKeyboard = mouseKeyboard;
+        this.joystick = joystick;
+    }
+
+    @Override
+    public void getDataSheet() {
+        String text = """
+                Título: %s
+                Gênero: %s
+                Ano de lançamento: %d
+                Nota: %.1f
+                Quantidade de avaliações: %d
+                Necessário assinatura: %b
+                Aceita mouse e teclado: %b
+                Aceita controle: %b
+                """.formatted(getTitle(), getGenre(), getLaunchYear(), getRatingAvarage(), getRatingsQuantity(), isSubscriptionNeeded(), isMouseKeyboard(), isJoystick());
+        System.out.println(text);
+    }
+
+    public boolean isMouseKeyboard() {
+        return mouseKeyboard;
+    }
+
+    public boolean isJoystick() {
+        return joystick;
+    }
+}
