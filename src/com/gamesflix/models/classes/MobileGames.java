15e376f767e3ee7a29cf21470c4b038e503d7beb
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2023-11-30 11:27:36 -0300

    Create MobileGames.java

---
+++ src/com/gamesflix/models/classes/MobileGames.java
@@ -0,0 +1,37 @@
+package com.gamesflix.models.classes;
+
+import com.gamesflix.models.interfaces.Rateable;
+
+public class MobileGames extends Games implements Rateable {
+    boolean androidAvailable;
+    boolean iosAvailable;
+
+    public MobileGames(String title, String genre, int launchYear, boolean subscriptionNeeded, boolean androidAvailable, boolean iosAvailable) {
+        super(title, genre, launchYear, subscriptionNeeded);
+        this.androidAvailable = androidAvailable;
+        this.iosAvailable = iosAvailable;
+    }
+
+    @Override
+    public void getDataSheet() {
+        String text = """
+                Título: %s
+                Gênero: %s
+                Ano de lançamento: %d
+                Nota: %.1f
+                Quantidade de avaliações: %d
+                Necessário assinatura: %b
+                Disponível para Android: %b
+                Disponível para iOS: %b
+                """.formatted(getTitle(), getGenre(), getLaunchYear(), getRatingAvarage(), getRatingsQuantity(), isSubscriptionNeeded(), isAndroidAvailable(), isIosAvailable());
+        System.out.println(text);
+    }
+
+    public boolean isAndroidAvailable() {
+        return androidAvailable;
+    }
+
+    public boolean isIosAvailable() {
+        return iosAvailable;
+    }
+}
